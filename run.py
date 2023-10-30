688b5fe29817cf000593729547ea26abf90f1a5d
douglas <douglasmaxton@gmail.com>
2023-10-30 19:02:20 +0000

    changed format

--- run.py
+++ run.py
@@ -1,28 +1,26 @@
-# import gspread
-# from google.oauth2.service_account import Credentials
-import json
+import gspread
+from google.oauth2.service_account import Credentials
 
-# import os
-
-# SCOPE = [
-#     "https://www.googleapis.com/auth/spreadsheets",
-#     "https://www.googleapis.com/auth/drive.file",
-#     "https://www.googleapis.com/auth/drive",
-# ]
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive",
+]
 
 
 # creds = json.load(open("creds.json"))
-# CREDS = Credentials.from_service_account_file(client_secret, scopes=scopes)
-# SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-# GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+CREDS = Credentials.from_service_account_file("./creds.json", scopes=SCOPE)
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 # gc = GSPREAD_CLIENT.open("love_sandwiches")
-
-dictionary = json.load(open("creds.json"))
 
 
 def main():
     print("The json file is")
-    print(dictionary)
+    gc = GSPREAD_CLIENT.open("love_sandwiches")
+
+    print(gc)
+    print("file opened")
 
 
 main()

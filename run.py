3a10ec41f390108a17ca65d99f832e400c5338f2
douglas <douglasmaxton@gmail.com>
2023-10-30 12:58:57 +0000

    added credentials and connect to sheets with gspread

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
+]
+
+credentials = Credentials.from_service_account_file("creds.json", scopes=scopes)
+
+gc = gspread.authorize(credentials)
+
+
+def main():
+    print("main program")
+    try:
+        gc.open("love_sandwiches")
+        print("file opened")
+    except gspread.SpreadsheetNotFound:
+        print("file not found")
+
+
+main()

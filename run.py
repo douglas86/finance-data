20562738bdcca2923971ca3f870fa8dc1541543e
douglas <douglasmaxton@gmail.com>
2023-10-30 18:04:12 +0000

    print the creds.json file out to the console

--- run.py
+++ run.py
@@ -1,27 +1,28 @@
-import gspread
-from google.oauth2.service_account import Credentials
+# import gspread
+# from google.oauth2.service_account import Credentials
 import json
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive",
-]
+# import os
 
-creds = json.load(open("creds.json"))
-CREDS = Credentials.from_service_account_file(creds)
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-gc = GSPREAD_CLIENT.open("love_sandwiches")
+# SCOPE = [
+#     "https://www.googleapis.com/auth/spreadsheets",
+#     "https://www.googleapis.com/auth/drive.file",
+#     "https://www.googleapis.com/auth/drive",
+# ]
+
+
+# creds = json.load(open("creds.json"))
+# CREDS = Credentials.from_service_account_file(client_secret, scopes=scopes)
+# SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+# GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+# gc = GSPREAD_CLIENT.open("love_sandwiches")
+
+dictionary = json.load(open("creds.json"))
 
 
 def main():
-    print("main program")
-    try:
-        gc.open("love_sandwiches")
-        print("file opened")
-    except gspread.SpreadsheetNotFound:
-        print("file not found")
+    print("The json file is")
+    print(dictionary)
 
 
 main()

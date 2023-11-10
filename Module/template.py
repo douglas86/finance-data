646af746b9c7c0b4b46fd3790e4412844d2806f4
douglas <douglasmaxton@gmail.com>
2023-11-10 11:23:23 +0000

    refactored code for crud operation with spreadsheet

--- Module/template.py
+++ Module/template.py
@@ -1,4 +1,121 @@
+from Module.settings import client
+
+from Module.dates import current_year
+
+
 class Template:
     """
     The template is still to be created
     """
+
+    list_data_from_sheet = []
+
+    def __init__(self, file_id, folder_id):
+        """
+        Initializes class with variables
+        :param file_id:
+        :param folder_id:
+        """
+        self.file_id = file_id
+        self.folder_id = folder_id
+
+    def __iter__(self, crud_operation, lists):
+        """
+        Special built-in function to iterate over a list, dictionary or tuple
+        :return:
+        """
+        # gets batch data from spreadsheet
+        if crud_operation == "get":
+            getting = self.open_spreadsheet().worksheet("data").batch_get(lists)
+            self.list_data_from_sheet.append(getting[0])
+            return f"Data fetched from {current_year} spreadsheet"
+        # updates batch data to spreadsheet
+        else:
+            self.open_spreadsheet().sheet1.batch_update(lists)
+            return f"Data has been updated to {current_year} spreadsheet"
+
+    def open_spreadsheet(self):
+        """
+        Opens up the spreadsheet
+        :return:
+        """
+        return client.open(title=str(current_year), folder_id=self.folder_id)
+
+    def get_data(self):
+        """
+        Calls __iter__ to iterate over data from spreadsheet
+        :return:
+        """
+        print(self.__iter__("get", ["D9:O24"]))
+
+    def update_data(self):
+        """
+        Once data has been fetched
+        Calls __iter__ method to iterate over and update data to spreadsheet
+        :return:
+        """
+
+        # list to update spreadsheet with
+        list_to_update_spreadsheet = [{"range": "A1", "values": [[int(current_year)]]}]
+
+        # variables to call with updating list above
+        acc_data_range = 5
+        acc_data_values = 0
+
+        # iteration to update the list above
+        while acc_data_range < 21:
+            list_to_update_spreadsheet.append(
+                {
+                    "range": f"BE{acc_data_range}:DP{acc_data_range}",
+                    "values": [
+                        self.list_data()[acc_data_values : acc_data_values + 12]
+                    ],
+                }
+            )
+            # acc_data_range updates by one as the rows are one after the other in spreadsheet
+            acc_data_range += 1
+            # acc_data_values updates by 12 as there are 12 months in a year
+            # and columns are one after the other
+            acc_data_values += 12
+
+        print(self.__iter__("updating", list_to_update_spreadsheet))
+
+    def list_data(self):
+        """
+        Combines multiple lists in a list to one complete list
+        :return:
+        """
+        return [
+            eval(x)
+            for i in range(len(self.list_data_from_sheet[0]))
+            for x in self.list_data_from_sheet[0][i]
+        ]
+
+    def create_spreadsheet(self):
+        """
+        Creates the spreadsheet
+        Gets and Updates data once the spreadsheet is created
+        :return:
+        """
+
+        # Copies template spreadsheet
+        # and creates a spreadsheet based on current_year
+        client.copy(
+            file_id=self.file_id,
+            title=current_year,
+            copy_permissions=True,
+            folder_id=self.folder_id,
+        )
+
+        print(f"{current_year} spreadsheet created")
+
+        # get and update data was spreadsheet has been created
+        self.get_data()
+        self.update_data()
+
+
+# variable to call the class Template
+template = Template(
+    file_id="1-M5rHQY78wq_VWDUWLhz2hZvux3lY9y7lOT7PY4uGS0",
+    folder_id="1jDCB53uToJ7nDm5_vxZr7019Zje9RmcX",
+)
--- testing/delete_spreadsheet.py
+++ testing/delete_spreadsheet.py
@@ -1,9 +1,11 @@
-def delete_spreadsheet(gspread_client, file_id):
+from Module.settings import GSPREAD_CLIENT
+
+
+def delete_spreadsheet(file_id):
     """
     Deletes spreadsheet by file_id
-    :param gspread_client:
     :param file_id:
     :return:
     """
-    gspread_client.del_spreadsheet(file_id=file_id)
+    GSPREAD_CLIENT.del_spreadsheet(file_id=file_id)
     print("File deleted")

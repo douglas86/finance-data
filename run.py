26e4b88cd0e2f3faba4f0ac65ea9c83d1fcb5444
douglas <douglasmaxton@gmail.com>
2023-11-15 21:10:29 +0000

    delete print statement and calling delete_spreadsheet function

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ from gspread.exceptions import SpreadsheetNotFound
 
 
 from Module.template import template
-from utils.keys import FOLDER_ID
 
 
 def main():
@@ -11,14 +10,8 @@ def main():
     :return:
     """
 
-    # Deleted spreadsheet
-    # delete_spreadsheet(
-    #     file_id="1v7RwDJVSBYEVyiQtEPTJcUDm90R4d4UQYF40g4lirG4",
-    # )
-
     # check to see if the spreadsheet with current_year is created
     try:
-        print("folder id", FOLDER_ID)
         template.open_spreadsheet()
         print("spreadsheet open")
     except SpreadsheetNotFound:

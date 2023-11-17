27ba270908779c6956dc3170a5d63a1a86c57c58
douglas <douglasmaxton@gmail.com>
2023-11-17 10:42:52 +0000

    deleted test function in tests.py

--- Module/tests.py
+++ Module/tests.py
@@ -1,4 +1,3 @@
-from Module.validators import Validators
 from Module.settings import GSPREAD_CLIENT
 
 
@@ -10,17 +9,3 @@ def delete_spreadsheet(file_id):
     """
     GSPREAD_CLIENT.del_spreadsheet(file_id=file_id)
     print("File deleted")
-
-
-def strip_away_pound():
-    # symbol = "£3,000.00"
-    data = []
-
-    while True:
-        number = input("Please enter your salary?\n")
-        validators = Validators(number=number, option=2)
-        correct_answer = validators.check_number()
-
-        if correct_answer:
-            data.append(float(number))
-            break
